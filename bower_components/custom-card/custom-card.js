eeb76b18a248879a4f212c6e2b825200300bbf26
Santiago Mejía Sánchez <Santiago Meja Snchez>
2016-01-08 07:59:18 -0500

    Implementación de componente issues-list (Neon Animations y Paper list)

--- bower_components/custom-card/custom-card.js
+++ bower_components/custom-card/custom-card.js
@@ -2,10 +2,22 @@ Polymer({
 	is: 'custom-card',
 	properties: {
 		nombre: String,
-		edad: Number
+		edad: Number,
+		employees: {
+          type: Array,
+          value: function() {
+            return [
+              { firstname: "Jack", lastname: "Aubrey" },
+              { firstname: "Anne", lastname: "Elliot" },
+              { firstname: "Stepehen", lastname: "Maturin" },
+              { firstname: "Emma", lastname: "Woodhouse" }
+            ]
+          }
+        }
 	},
 	ready: function(){
-		console.log("Custom card ready");
+		console.log("Custom card readyy");
 		this.nombre = "Santiago";
+		console.log( this.employees );
 	}
 });
